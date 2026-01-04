b517b7c20e9957b8858f762ae3fc927ac49a1428
Craig Fisher <140781290+craigfisher-dev@users.noreply.github.com>
2026-01-04 00:23:16 -0500

    Add interactive team schedule calendar with logos
    Replaces placeholder calendar code with a dynamic, interactive calendar for each MLB team, displaying scheduled games and opponent logos for each month. Adds helper functions to process and render the schedule, merges March and April as needed, and improves the sidebar and main layout to show the selected team's schedule in a visually appealing table format.

--- app.py
+++ app.py
@@ -19,6 +19,9 @@ ESPN_API_URL = "https://site.api.espn.com/apis/site/v2/sports/baseball/mlb/teams
 
 CURRENT_YEAR = 2026
 
+# Set Sunday as first day of week
+calendar.setfirstweekday(calendar.SUNDAY)
+
 st.set_page_config("MLB Game Schedule Analyzer", layout="wide")
 
 # Transforms a teams regular season schedule into 6 lists
@@ -104,16 +107,18 @@ def fetch_all_team_logos():
     
     # Turned it to json to be able to access it as a dictionary
     data = response.json()
-
     teams = data["sports"][0]["leagues"][0]["teams"]
-    logos = []
+    
+    logo_map = {}
 
     # Loop through all teams and add in their logos
     for team in teams:
         # Transparent logos (href 4 on ESPNs API list)
-        logos.append(team["team"]["logos"][4]["href"])
+        team_name = team["team"]["displayName"]
+        logo_url = team["team"]["logos"][4]["href"]
+        logo_map[team_name] = logo_url
     
-    return logos
+    return logo_map
         
 
 # Load schedules and logos in parallel for faster startup
@@ -123,7 +128,7 @@ with st.spinner("Loading..."):
         logos_future = executor.submit(fetch_all_team_logos)
         
         all_schedules = schedules_future.result()
-        team_logos = logos_future.result()
+        team_logo_map = logos_future.result()
 
 
 # Shows the links collected from ESPNs API
@@ -140,6 +145,179 @@ if teamName:
 else:
     st.markdown("<h1 style='text-align: center;'>MLB Game Schedule Analyzer</h1>", unsafe_allow_html=True)
 
+
+
+# Helper function to process_month
+def process_month(month_calendar, month_games, teamName):
+    left_ptr = 0
+    right_ptr = 0
+    
+    # Flatten to 1D
+    month_flat = [day for week in month_calendar for day in week]
+
+    while left_ptr < len(month_flat):
+        if month_flat[left_ptr] == 0:
+            month_flat[left_ptr] = (-1, None)  # Padding day
+            left_ptr += 1
+
+        elif right_ptr < len(month_games) and month_flat[left_ptr] == int(month_games[right_ptr]["game_date"][8:10]):
+            home_team = month_games[right_ptr]["home_name"]
+            away_team = month_games[right_ptr]["away_name"]
+            teamPlaying = away_team if teamName == home_team else home_team
+
+            day_num = month_flat[left_ptr]
+            month_flat[left_ptr] = (day_num, teamPlaying)  # (day, opponent)
+            
+            # Skip any additional games on the same day (doubleheaders)
+            current_date = month_games[right_ptr]["game_date"]
+            right_ptr += 1
+            while right_ptr < len(month_games) and month_games[right_ptr]["game_date"] == current_date:
+                right_ptr += 1
+            
+            left_ptr += 1
+        else:
+            day_num = month_flat[left_ptr]
+            month_flat[left_ptr] = (day_num, None)  # No game
+            left_ptr += 1
+
+    # Reshape back to 2D
+    return [month_flat[i:i+7] for i in range(0, len(month_flat), 7)]
+
+
+def trim_empty_weeks(month_data, trim_start=False, trim_end=False):
+    result = month_data[:]
+    
+    # Trim leading weeks with no games
+    if trim_start:
+        while result and all(opponent is None for day_num, opponent in result[0]):
+            result = result[1:]
+    
+    # Trim trailing weeks with no games
+    if trim_end:
+        while result and all(opponent is None for day_num, opponent in result[-1]):
+            result = result[:-1]
+    
+    return result
+
+# If a team is chosen then display the teams schedule
+
+# To access a team game 
+# month[0-5]
+# [0 = March/April, 1 = May, 2 = June, 3 = July, 4 = August, 5 = September]
+
+# all_schedules[teamName]['by_month'][month][game_number]
+
+def print_team_calendar(teamName):
+    
+    march_april_games = all_schedules[teamName]['by_month'][0]
+    
+    march_games = [g for g in march_april_games if g["game_date"][5:7] == "03"]
+    april_games = [g for g in march_april_games if g["game_date"][5:7] == "04"]
+    
+    march = calendar.monthcalendar(year=CURRENT_YEAR, month=3)
+    april = calendar.monthcalendar(year=CURRENT_YEAR, month=4)
+    
+    march_processed = process_month(march, march_games, teamName)
+    april_processed = process_month(april, april_games, teamName)
+    
+    # Only trim start of March
+    march_processed = trim_empty_weeks(march_processed, trim_start=True)
+
+    # Hide days before first game in the remaining first week
+    if march_games and march_processed:
+        first_game_day = int(march_games[0]["game_date"][8:10])
+        new_first_week = []
+        for day_num, opponent in march_processed[0]:
+            if day_num != -1 and day_num < first_game_day:
+                new_first_week.append((-1, None))
+            else:
+                new_first_week.append((day_num, opponent))
+        march_processed[0] = new_first_week
+    
+    # Merge last week of March with first week of April if they share a row
+    if march_processed and april_processed:
+        last_march_week = march_processed[-1]
+        first_april_week = april_processed[0]
+        
+        merged_week = []
+        can_merge = True
+        for i in range(7):
+            march_day = last_march_week[i]
+            april_day = first_april_week[i]
+            
+            if march_day[0] == -1 and april_day[0] == -1:
+                merged_week.append((-1, None))
+            elif march_day[0] == -1:
+                merged_week.append(april_day)
+            elif april_day[0] == -1:
+                merged_week.append(march_day)
+            else:
+                can_merge = False
+                break
+        
+        if can_merge:
+            march_processed = march_processed[:-1]
+            april_processed = april_processed[1:]
+            march_april = march_processed + [merged_week] + april_processed
+        else:
+            march_april = march_processed + april_processed
+    else:
+        march_april = march_processed + april_processed
+
+    # Don't trim other months - just process them
+    may = process_month(calendar.monthcalendar(CURRENT_YEAR, 5), all_schedules[teamName]['by_month'][1], teamName)
+    june = process_month(calendar.monthcalendar(CURRENT_YEAR, 6), all_schedules[teamName]['by_month'][2], teamName)
+    july = process_month(calendar.monthcalendar(CURRENT_YEAR, 7), all_schedules[teamName]['by_month'][3], teamName)
+    august = process_month(calendar.monthcalendar(CURRENT_YEAR, 8), all_schedules[teamName]['by_month'][4], teamName)
+    september = process_month(calendar.monthcalendar(CURRENT_YEAR, 9), all_schedules[teamName]['by_month'][5], teamName)
+    
+    # Only trim end of September (after season ends)
+    september = trim_empty_weeks(september, trim_end=True)
+
+    team_calendar = [march_april, may, june, july, august, september]
+    
+    return team_calendar
+
+# Testing print_team_calendar
+print()
+print(print_team_calendar(teamName))
+
+
+
+def render_month_calendar_html(month_data):
+    html = "<table style='width:100%; text-align:center; border-collapse:collapse; border:none;'>"
+    html += "<tr><th style='border:none;'>Sun</th><th style='border:none;'>Mon</th><th style='border:none;'>Tue</th><th style='border:none;'>Wed</th><th style='border:none;'>Thu</th><th style='border:none;'>Fri</th><th style='border:none;'>Sat</th></tr>"
+    
+    for week in month_data:
+        # Skip rows that are all padding
+        if all(day[0] == -1 for day in week):
+            continue
+            
+        html += "<tr>"
+        for day_num, opponent in week:
+            if day_num == -1:
+                # Invisible cell
+                html += "<td style='border:none; background:none;'></td>"
+            elif opponent:  # Game day
+                logo_url = team_logo_map.get(opponent, "")
+                if logo_url:
+                    html += f"""<td style='padding:2px; vertical-align:top; border:1px solid #333;'>
+                        <div style='text-align:left; font-size:9px; color:#888;'>{day_num}</div>
+                        <img src='{logo_url}' width='20' title='{opponent}'>
+                    </td>"""
+                else:
+                    html += f"""<td style='padding:2px; vertical-align:top; border:1px solid #333;'>
+                        <div style='text-align:left; font-size:9px; color:#888;'>{day_num}</div>
+                        {opponent[:3]}
+                    </td>"""
+            else:
+                # No game - just show day number
+                html += f"<td style='padding:2px; vertical-align:top; border:1px solid #333;'><div style='text-align:left; font-size:9px; color:#888;'>{day_num}</div></td>"
+        html += "</tr>"
+    
+    html += "</table>"
+    return html
+
 # In the sidebar
 with st.sidebar:
     with st.container():
@@ -148,7 +326,7 @@ with st.sidebar:
             team_schedule = all_schedules[teamName]
 
             # st.write(team_schedule['games'][:5])  # Show first 5 games
-            st.write(team_schedule['by_month'][0][:2])   # Shows the first 2 March/April games
+            st.write(team_schedule['by_month'][0][:2])   # Shows the first two games of March/April
 
             # Team logo displayed
             
@@ -165,35 +343,28 @@ with st.sidebar:
 c1, c2, c3  = st.columns(3)
 c4, c5, c6  = st.columns(3)
 
-with st.container():
-    c1.markdown(f"<h3 style='text-align: center;'>March/April</h3>", unsafe_allow_html=True)
-    # PlaceHolder Calendar Month - Will be replaced with real one after it is implemented
-    # Needs to be both march and april right now its just april
-    march_april = calendar.month(CURRENT_YEAR, 4)
-    c1.code(march_april, language=None, width="content")
+if teamName:
+    team_calendar = print_team_calendar(teamName)
+    
+    if team_calendar:
+        with st.container():
+            c1.markdown("<h3 style='text-align: center;'>March/April</h3>", unsafe_allow_html=True)
+            c1.markdown(render_month_calendar_html(team_calendar[0]), unsafe_allow_html=True)
 
-    c2.markdown(f"<h3 style='text-align: center;'>May</h3>", unsafe_allow_html=True)
-    # PlaceHolder Calendar Month - Will be replaced with real one after it is implemented
-    may = calendar.month(CURRENT_YEAR, 5)
-    c2.code(may, language=None, width="content")
+            c2.markdown("<h3 style='text-align: center;'>May</h3>", unsafe_allow_html=True)
+            c2.markdown(render_month_calendar_html(team_calendar[1]), unsafe_allow_html=True)
 
-    c3.markdown(f"<h3 style='text-align: center;'>June</h3>", unsafe_allow_html=True)
-    # PlaceHolder Calendar Month - Will be replaced with real one after it is implemented
-    june = calendar.month(CURRENT_YEAR, 6)
-    c3.code(june, language=None, width="content")
+            c3.markdown("<h3 style='text-align: center;'>June</h3>", unsafe_allow_html=True)
+            c3.markdown(render_month_calendar_html(team_calendar[2]), unsafe_allow_html=True)
 
-with st.container():
-    c4.markdown(f"<h3 style='text-align: center;'>July</h3>", unsafe_allow_html=True)
-    # PlaceHolder Calendar Month - Will be replaced with real one after it is implemented
-    july = calendar.month(CURRENT_YEAR, 7)
-    c4.code(july, language=None, width="content")
+        with st.container():
+            c4.markdown("<h3 style='text-align: center;'>July</h3>", unsafe_allow_html=True)
+            c4.markdown(render_month_calendar_html(team_calendar[3]), unsafe_allow_html=True)
 
-    c5.markdown(f"<h3 style='text-align: center;'>August</h3>", unsafe_allow_html=True)
-    # PlaceHolder Calendar Month - Will be replaced with real one after it is implemented
-    august = calendar.month(CURRENT_YEAR, 8)
-    c5.code(august, language=None, width="content")
+            c5.markdown("<h3 style='text-align: center;'>August</h3>", unsafe_allow_html=True)
+            c5.markdown(render_month_calendar_html(team_calendar[4]), unsafe_allow_html=True)
 
-    c6.markdown(f"<h3 style='text-align: center;'>September</h3>", unsafe_allow_html=True)
-    # PlaceHolder Calendar Month - Will be replaced with real one after it is implemented
-    september = calendar.month(CURRENT_YEAR, 9)
-    c6.code(september, language=None, width="content")
+            c6.markdown("<h3 style='text-align: center;'>September</h3>", unsafe_allow_html=True)
+            c6.markdown(render_month_calendar_html(team_calendar[5]), unsafe_allow_html=True)
+    else:
+        st.error("Failed to fetch team_calendar")
